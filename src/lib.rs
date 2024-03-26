446013444205c84d0c5e3603388da146fb49db8c
Leandro Reina <leandro.reina@ororatech.com>
2024-03-27 00:19:27 +0100

    Move process function to its own file

--- src/lib.rs
+++ src/lib.rs
@@ -20,52 +20,5 @@ pub mod fonts;
 pub mod help;
 pub mod meta;
 
-pub use self::{colour::COLOURS, cp437::*, meta::Meta};
-
-use std::{
-    fs::File,
-    io::{stdout, Write},
-    process::ExitCode,
-};
-
-#[doc(hidden)]
-#[inline]
-pub fn process<
-    'a,
-    F: for<'b> FnOnce(&'b mut File, &'b mut Box<dyn Write>, Option<Meta>) -> Result<(), String> + 'a,
->(
-    input: &String,
-    output: &Option<String>,
-    callback: F,
-) -> ExitCode {
-    match wrapped_process(input, output, callback) {
-        Ok(_) => {
-            return ExitCode::from(0);
-        }
-        Err(msg) => {
-            eprintln!("\x1B[31mERROR: {}\x1B[0m", msg);
-            return ExitCode::from(2);
-        }
-    }
-}
-
-#[inline]
-fn wrapped_process<
-    'a,
-    F: for<'b> FnOnce(&'b mut File, &'b mut Box<dyn Write>, Option<Meta>) -> Result<(), String> + 'a,
->(
-    input: &String,
-    output: &Option<String>,
-    callback: F,
-) -> Result<(), String> {
-    let mut input = File::open(input).map_err(|x| return x.to_string())?;
-    let meta = meta::read(&mut input)?;
-    meta::check(&meta)?;
-    let mut output = match output {
-        Some(filename) => Box::new(File::create_new(filename).map_err(|x| return x.to_string())?)
-            as Box<dyn Write>,
-        None => Box::new(stdout()) as Box<dyn Write>,
-    };
-
-    return callback(&mut input, &mut output, meta);
-}
+mod process;
+pub use self::{colour::COLOURS, cp437::*, meta::Meta, process::*};
